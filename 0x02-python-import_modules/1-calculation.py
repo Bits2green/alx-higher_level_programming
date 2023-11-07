de7428dbfca032c131f4cc2ca38a3b49b18f9bd0
Bits2green <neshmunesh2@gmail.com>
2023-11-07 04:54:26 +0000

    add calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+
 from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
-sums = add(a, b)
-diff = abs(sub(a, b))
-prod = mul(a, b)
-quo = div(a, b)
-
-print(f"sum = {sums}, difference = {diff}, product = {prod}, quotient = {quo}")
-
+print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
+print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))

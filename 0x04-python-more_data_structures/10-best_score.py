2136ffca7458977ac9b5a6c675185534b3278e8e
Bits2green <neshmunesh2@gmail.com>
2023-11-08 17:17:30 +0300

    fr

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict and len(my_dict):
-        max = list(my_dict.keys())[0]
-        for k in my_dict:
-            if my_dict[k] > my_dict[max]:
-                max = k
-        return max
-    return None
+if my_dict and len(my_dict):
+max = list(my_dict.keys())[0]
+for k in my_dict:
+if my_dict[k] > my_dict[max]:
+max = k
+return max
+return None

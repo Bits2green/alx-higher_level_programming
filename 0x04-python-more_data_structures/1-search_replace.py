1b0777ff6c054b415bf64d3ad771cca3ab8222d5
Bits2green <neshmunesh2@gmail.com>
2023-11-08 17:20:02 +0300

    fr

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-def sr(sr):
-return (sr if sr != search else replace)
-return list(map(sr, my_list))
+    def sr(sr):
+        return (sr if sr != search else replace)
+    return list(map(sr, my_list))
--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-if my_list and len(my_list):
-y = 0
-x = 0
-for i in my_list:
-y += (i[0] * i[1])
-x += i[1]
-return (y / x)
-return 0
+    if my_list and len(my_list):
+        y = 0
+        x = 0
+        for tup in my_list:
+            y += (tup[0] * tup[1])
+            x += tup[1]
+        return (y / x)
+    return 0

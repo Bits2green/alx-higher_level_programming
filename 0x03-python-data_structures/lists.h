599b13335c68c1346ae5fb50afc5cfb4ac86688d
Bits2green <neshmunesh2@gmail.com>
2023-11-09 21:23:50 +0300

    double

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -10,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ *
  */
 typedef struct listint_s
 {
@@ -18,9 +17,12 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-int is_palindrome(listint_t **head);
-listint_t *add_nodeint_end(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int is_palindrome(listint_t **head);
+size_t listint_len(const listint_t *h);
 
 #endif

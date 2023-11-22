829fb3333292eb14ca87d920ca88d4ffb0b2585f
Bits2green <neshmunesh2@gmail.com>
2023-11-22 09:21:14 +0300

    fee

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes the square
+        Args:
+            size (int): side of the square
+        Returns: None
+        """
+        self.__size = size

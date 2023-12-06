72e61367c8ad6cb9575fa9c215857e9c0a923383
Bits2green <neshmunesh2@gmail.com>
2023-12-06 08:11:27 +0300

    :I/O

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains the "append after" function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appends "new_string" after a line containing
+    "search_string" in "filename" """
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines_l = []
+        while True:
+            lines_print = f.readline()
+            if lines_print == "":
+                break
+            lines_l.append(lines_print)
+            if search_string in lines_print:
+                lines_l.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writeline(lines_l)

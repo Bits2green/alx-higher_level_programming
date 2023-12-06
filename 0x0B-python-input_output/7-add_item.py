77693887bf4905e0e1639fc0d4dd47727b4fb72e
Bits2green <neshmunesh2@gmail.com>
2023-12-06 13:07:53 +0000

    i/o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 filename = "add_item.json"
 
 try:
-    json_list = load_from_json_file(f_name)
+    json_list = load_from_json_file(filename)
 except FileNotFoundError:
     json_list = []
 
